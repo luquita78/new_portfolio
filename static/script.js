58b51f6d5b309084bdfd18566fbebc2628c5ea65
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2022-04-10 17:19:10 -0300

    Menu mobile e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,16 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x","bi-list");
     body.classList.toggle("menu-nav-active");
 });
+
+/*Comando para fechar o menu quando clicar em algum item *menu_mobile */
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
