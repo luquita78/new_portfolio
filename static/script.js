ff2715555a7b9a03db594c57232305b889097f9b
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2022-04-12 15:57:37 -0300

    Animações pelo site

--- static/script.js
+++ static/script.js
@@ -22,3 +22,27 @@ navItem.forEach(item => {
         }
     })
 })
+
+/* ANIMAÇÃO */
+
+const item = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () =>{
+    const janelaTopo = window.pageYOffset + window.innerHeight *0.85;
+
+
+    item.forEach((element) => {
+        if (janelaTopo > element.offsetTop){
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+
+}
+
+animeScroll();
+
+window.addEventListener("scroll", () =>{
+    animeScroll();
+})
